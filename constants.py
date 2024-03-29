ab34a7fc6ceafeac431a18d5db661497e5dcf680
Preetham Manjunatha <preethamam@gmail.com>
2024-03-28 23:06:29 -0700

    Added Python files

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Depth scale (constant) to convert mm to m vice-versa
+DEPTH_SCALE = 1000
+
+# JSON file of camera intrinsics
+JSON_FILE = "camera.json"
+
+# Output file name
+PCD_FILENAME = "output_py.pcd"
